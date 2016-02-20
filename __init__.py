b734e681703c4a856ba1fe0024b1c73cc4933943
mantaleigh <samantha@samanthavoigt.com>
2016-02-20 23:34:49 +0000

    File reorg

--- __init__.py
+++ __init__.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 @app.route('/')
-def hello(): 
-	return render_template('home.html', title="Home", user="testuser")
+def index(): 
+	return render_template('index.html')
+
+@app.route('/searchSchool', methods=['POST']) 
+def testing(): 
+	return render_template('test.html', school=request.form['school'])
+
 if __name__ == "__main__": 
 	app.run(debug=True)
+
